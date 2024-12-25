de2f0b87192639d7a60add70c85be92a1ec4d561
Dhanushmathan <lovelymathan2394@gmail.com>
2024-12-25 11:55:43 +0530

    fix: app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -83,7 +83,7 @@ const App = () => {
         </section>
 
         {/* Display values here */}
-        <section>
+        <section className="bg-white p-5 mt-2 rounded shadow">
           <div className="relative overflow-x-auto rounded">
             <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
               <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
